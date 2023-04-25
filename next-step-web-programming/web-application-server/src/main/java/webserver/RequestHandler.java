d12753afa0d84db1bbd449847bb9a4dcf2aef34d
Gyeongsu Kim <rla04722@naver.com>
2023-04-25 14:20:52 +0900

    Feat : 요구사항 1단계
    http://localhost:8080/index.html로 접속시 응답

--- next-step-web-programming/web-application-server/src/main/java/webserver/RequestHandler.java
+++ next-step-web-programming/web-application-server/src/main/java/webserver/RequestHandler.java
@@ -1,17 +1,14 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
-
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
@@ -23,9 +20,20 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            InputStreamReader read = new InputStreamReader(in, "UTF-8");
+            BufferedReader br = new BufferedReader(read); // 헤더 값
+
+            String token = br.readLine();
+            log.info("ㅡ url", token);
+
+            if (token == null) return; // 헤더가 null일 경우 응답x
+
+            // byte array로 변환 후 body에 넣어줌
+            byte[] body = Files.readAllBytes(new File("./webapp" + url(token)).toPath());
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -51,5 +59,18 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private static String url(String token) {
+        // 헤더 첫번째 줄 기준으로 자름
+        String[] splits = token.split(" ");
+
+        // url 값
+        String tokens = splits[1];
+
+        log.debug("\n0 : {}\n1 : {}\n2 : {}\n", splits[0], splits[1], splits[2]);
+        log.debug("ㅡ token {}", tokens);
+
+        return tokens;
     }
 }
