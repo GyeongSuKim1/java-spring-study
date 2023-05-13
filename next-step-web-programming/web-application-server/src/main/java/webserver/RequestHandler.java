47db9cb303e300bad39574eea72bd6ed7d89928e
Gyeongsu Kim <rla04722@naver.com>
2023-05-14 03:02:41 +0900

    Feat : 요구사항 1

--- next-step-web-programming/web-application-server/src/main/java/webserver/RequestHandler.java
+++ next-step-web-programming/web-application-server/src/main/java/webserver/RequestHandler.java
@@ -2,6 +2,7 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,6 +27,8 @@ public class RequestHandler extends Thread {
             String line = br.readLine();
             log.debug("request line : {}", line);
 
+            String[] tokens = line.split(" ");
+
             if(line == null) return;
 
             while (!line.equals("")) {
@@ -34,7 +37,7 @@ public class RequestHandler extends Thread {
             }
 
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
