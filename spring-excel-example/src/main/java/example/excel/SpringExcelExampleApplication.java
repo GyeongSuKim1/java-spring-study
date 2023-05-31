330bf6c3f9039098f5447559c4f7f9dec4423664
Gyeongsu Kim <rla04722@naver.com>
2023-05-31 14:13:38 +0900

    init project
    excel

---
+++ spring-excel-example/src/main/java/example/excel/SpringExcelExampleApplication.java
@@ -0,0 +1,13 @@
+package example.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExcelExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExcelExampleApplication.class, args);
+    }
+
+}
---
+++ spring-excel-example/src/test/java/example/excel/SpringExcelExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package example.excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExcelExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
