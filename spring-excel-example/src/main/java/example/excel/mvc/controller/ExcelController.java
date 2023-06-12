f1a8039722c20a4423b917604cf2fd15f5c2bf5b
GyeongSuKim <rla04722@naver.com>
2023-06-12 23:42:50 +0900

    Feat : ArticleListExcel생성

--- spring-excel-example/src/main/java/example/excel/mvc/controller/ExcelController.java
+++ spring-excel-example/src/main/java/example/excel/mvc/controller/ExcelController.java
@@ -1,5 +1,9 @@
 package example.excel.mvc.controller;
 
+import example.excel.domain.entity.Article;
+import example.excel.mvc.service.ArticleService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -13,9 +17,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Controller
+@Slf4j
+@AllArgsConstructor
 public class ExcelController {
+
+    private ArticleService articleService;
 
     // 예제 1
     @RequestMapping(value = "/excel/download", method = RequestMethod.GET)
@@ -70,13 +79,69 @@ public class ExcelController {
         wb.close();
     }
 
-    // 예제 2
-    @RequestMapping(value = "/excel/download/v2", method = RequestMethod.GET)
-    public void excelDownloadV2(HttpServletResponse response) {
-
+    @RequestMapping(value = "/article/excel", method = RequestMethod.GET)
+    public void articleExcel(HttpServletResponse response) throws IOException {
         Workbook wb = new XSSFWorkbook();
         Sheet sheet = wb.createSheet();
 
+        Row row = null;
+        Cell cell = null;
+        int rowIndex = 0;
+
+        LocalDate now = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formatedNow = now.format(formatter);
+
+        row = sheet.createRow(rowIndex++);
+        cell = row.createCell(0);
+        cell.setCellValue("번호");
+
+        cell = row.createCell(1);
+        cell.setCellValue("제목");
+
+        cell = row.createCell(2);
+        cell.setCellValue("설명");
+
+        cell = row.createCell(3);
+        cell.setCellValue("작성시간");
+
+        row = sheet.createRow(rowIndex++);
+        cell = row.createCell(5);
+        cell.setCellValue("다운로드 날짜");
+
+        row = sheet.createRow(rowIndex++);
+        cell = row.createCell(5);
+        cell.setCellValue(formatedNow);
+
+        List<Article> articleList = articleService.articleList();
+        log.info("ㅡ {}", articleList);
+
+        for (Article i : articleList) {
+            row = sheet.createRow(1 + rowIndex++);
+            log.info("1ㅡ {}", i);
+            log.info("2ㅡ {}", i.getIdx());
+            cell = row.createCell(0);
+            cell.setCellValue(i.getIdx());
+
+            cell = row.createCell(1);
+            cell.setCellValue(i.getTitle());
+
+            cell = row.createCell(2);
+            cell.setCellValue(i.getContent());
+
+            cell = row.createCell(3);
+            cell.setCellValue(i.getCreated_at().format(formatter));
+        }
+
+
+        // 컨텐츠 타입과 파일명 지정
+        response.setContentType("ms-vnd/excel");
+//        response.setHeader("Content-Disposition", "attachment;filename=example.xls");
+        response.setHeader("Content-Disposition", "attachment;filename=Article.xlsx");
+
+        // Excel File Output
+        wb.write(response.getOutputStream());
+        wb.close();
     }
 
 }
