c99f9c49fe768e6135049edb3368236e21fcd482
Gyeongsu Kim <rla04722@naver.com>
2023-06-20 09:32:51 +0900

    Init Project

---
+++ spring-introductory/src/main/java/hello/spring/SpringIntroductoryApplication.java
@@ -0,0 +1,13 @@
+package hello.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroductoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntroductoryApplication.class, args);
+    }
+
+}
