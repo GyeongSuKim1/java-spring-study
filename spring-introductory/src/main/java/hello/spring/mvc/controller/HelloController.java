586058b5368765970d78ef2264c2fd03c3dbdf71
Gyeongsu Kim <rla04722@naver.com>
2023-06-21 15:59:06 +0900

    Feat : MVC 템플릿 엔진

--- spring-introductory/src/main/java/hello/spring/mvc/controller/HelloController.java
+++ spring-introductory/src/main/java/hello/spring/mvc/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -14,5 +15,13 @@ public class HelloController {
 
         model.addAttribute("data", "hello~!");
         return "hello";
+    }
+
+    @RequestMapping(value = "/hello-mvc", method = RequestMethod.GET)
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+
+        model.addAttribute("name", name);
+
+        return "hello-template";
     }
 }
