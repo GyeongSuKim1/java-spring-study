fdd5fe9f43cfb83cf79cbaf423c6d43c4ec828bf
Gyeongsu Kim <rla04722@naver.com>
2023-06-21 17:48:20 +0900

    Remove : 삭제 후 branch로 생성

--- spring-excel-example/src/main/java/example/excel/mvc/repository/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package example.excel.mvc.repository;
-
-import example.excel.domain.entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> { }
