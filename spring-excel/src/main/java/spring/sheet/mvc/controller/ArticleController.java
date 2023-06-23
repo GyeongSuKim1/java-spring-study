96a0ab9730c20ffdf37da9fbfee4bd549f5c84f1
Gyeongsu Kim <rla04722@naver.com>
2023-06-23 11:29:56 +0900

    Feat : Excel예제 View

---
+++ spring-excel/src/main/java/spring/sheet/mvc/controller/ArticleController.java
@@ -0,0 +1,19 @@
+package spring.sheet.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ArticleController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView home() {
+        ModelAndView mav = new ModelAndView("index");
+
+
+
+        return mav;
+    }
+}
