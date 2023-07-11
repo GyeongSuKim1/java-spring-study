c35379967efe83a41f4eae1a7d941e73ab0ca958
Gyeongsu Kim <rla04722@naver.com>
2023-07-11 18:25:52 +0900

    Feat : 월력으로 변경

--- spring-excel/src/main/resources/static/js/comm.js
+++ spring-excel/src/main/resources/static/js/comm.js
@@ -1,7 +1,22 @@
+// $(function () {
+//     $("#datePicker").datepicker({
+//         dateFormat: "yy-mm-dd",
+//         maxDate: 0,
+//         minDate: new Date('2018-01-01')
+//     });
+// });
+
 $(function () {
-    $("#datePicker").datepicker({
-        dateFormat: "yy-mm-dd",
+    $("#datePicker").monthpicker({
+        dateFormat: "yy-mm",
         maxDate: 0,
-        minDate: new Date('2018-01-01')
+        minDate: "-3Y",
+        monthNames: ['','','','','','','','','','','',''],
+        monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        // showOn: "button",
+        // buttonImage: "📅",
+        // buttonImageOnly: true,
+        // changeYear: false,
+        // yearRange: 'c-2:c+2',
     });
 });
