81949e530fb028831b3068b7306e6a484e632ccc
Gyeongsu Kim <rla04722@naver.com>
2023-08-21 10:32:05 +0900

    Feat (test) : 회원 가입 테스트

---
+++ spring-basic/src/test/java/hello/core/service/MemberServiceTest.java
@@ -0,0 +1,29 @@
+package hello.core.service;
+
+import hello.core.domain.entity.Member;
+import hello.core.domain.type.Grade;
+import hello.core.service.impl.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    @DisplayName("회원 가입 테스트")
+    void join() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        // then
+        assertEquals(member, findMember);
+    }
+}
