6a731796b125dd75c5a9e43e6923ab4a2cb26cb9
Gyeongsu Kim <rla04722@naver.com>
2023-08-21 10:44:42 +0900

    Refactor (test) : 불필요한 junit4 코드 삭제

--- spring-basic/src/test/java/hello/core/member/MemberServiceTest.java
+++ spring-basic/src/test/java/hello/core/member/MemberServiceTest.java
@@ -7,7 +7,6 @@ import hello.core.service.impl.MemberServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
