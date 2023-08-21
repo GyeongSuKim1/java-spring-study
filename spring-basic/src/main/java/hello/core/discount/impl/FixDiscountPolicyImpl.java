f081b3afd719a99e794b34e1976e0311ed984912
Gyeongsu Kim <rla04722@naver.com>
2023-08-21 16:27:42 +0900

    Refactor (discount) : Fix, Rate로 분리
    기존 DiscountPolicy를 Fix, Rate로 분리

--- spring-basic/src/main/java/hello/core/discount/impl/FixDiscountPolicyImpl.java
+++ spring-basic/src/main/java/hello/core/discount/impl/FixDiscountPolicyImpl.java
@@ -4,15 +4,15 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.domain.entity.Member;
 import hello.core.domain.type.Grade;
 
-public class DiscountPolicyImpl implements DiscountPolicy {
+public class FixDiscountPolicyImpl implements DiscountPolicy {
 
-    private int discountFixAmount = 10; // 10% 할인 금액
+    private int discountFixAmount = 1000; // 할인 금액
 
     @Override
     public int discount(Member member, int price) {
 
         if (member.getGrade() == Grade.VIP) {
-            return price * discountFixAmount / 100;
+            return discountFixAmount;
         } else {
             return 0;
         }
---
+++ spring-basic/src/main/java/hello/core/discount/impl/RateDiscountPolicyImpl.java
@@ -0,0 +1,20 @@
+package hello.core.discount.impl;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.domain.entity.Member;
+import hello.core.domain.type.Grade;
+
+public class RateDiscountPolicyImpl implements DiscountPolicy {
+
+    private int discountFixAmount = 10; //10% 할인 금액
+
+    @Override
+    public int discount(Member member, int price) {
+
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountFixAmount / 100;
+        } else {
+            return 0;
+        }
+    }
+}
