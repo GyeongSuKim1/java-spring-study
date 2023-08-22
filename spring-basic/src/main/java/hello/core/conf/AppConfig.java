76ee41dad01b36443c658589944aad766e2b5329
Gyeongsu Kim <rla04722@naver.com>
2023-08-22 15:55:26 +0900

    Refactor (config) : Spring기반으로 변경
    - Spring의 기능을 사용하여 Bean등록
    
    - 스프링 컨테이너에 객체를 스프링 빈으로 등록하고, 스프링 컨테이너에서 스프링 빈을 찾아서 사용하도록 변경

--- spring-basic/src/main/java/hello/core/conf/AppConfig.java
+++ spring-basic/src/main/java/hello/core/conf/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core.conf;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.impl.FixDiscountPolicyImpl;
 import hello.core.discount.impl.RateDiscountPolicyImpl;
 import hello.core.repository.MemberRepository;
 import hello.core.repository.impl.MemberRepositoryImpl;
@@ -9,13 +8,18 @@ import hello.core.service.MemberService;
 import hello.core.service.OrderService;
 import hello.core.service.impl.MemberServiceImpl;
 import hello.core.service.impl.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
@@ -23,10 +27,12 @@ public class AppConfig {
         );
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemberRepositoryImpl();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicyImpl();
         return new RateDiscountPolicyImpl();
