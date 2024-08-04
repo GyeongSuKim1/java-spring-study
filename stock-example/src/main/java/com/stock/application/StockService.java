b35fafea5e3b6edba2af2425adb4f4f3258e7f08
Gyeongsu Kim <89643366+GyeongSuKim1@users.noreply.github.com>
2024-08-04 17:49:43 +0900

    Docs : 주석 변경

--- stock-example/src/main/java/com/stock/application/StockService.java
+++ stock-example/src/main/java/com/stock/application/StockService.java
@@ -19,7 +19,7 @@ public class StockService {
      * @synchronized 를 사용 했을때 생기는 문제점
      * JAVA 의 @synchronized 는 하나의 프로세스안에서만 보장을 해줌, 서버가 한대일 경우 데이터의 접근을 서버가 한대만 해서 괜찮지만 서버가 여러개일 경우
      * 데이터의 접근을 여러개가 할 수 있음.
-     * 실제 운영하는 서비스는 대부분 두대 이상의 서버를 사용하기 때문에 @synchronized 는 적절하지 않음.
+     * 실제 운영하는 서비스는 대부분 두대 이상의 서버를 사용하기 때문에 @synchronized 는 거의 사용하지 않음.
      */
     public synchronized void decrease(final Long id, final Long quantity) {
         final Stock stock = stockRepository.findById(id).orElseThrow();
