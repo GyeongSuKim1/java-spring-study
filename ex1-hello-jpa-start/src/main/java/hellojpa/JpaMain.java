1a5c0f5a28e7cd652bb1de446416573716c56ee5
Gyeongsu Kim <89643366+GyeongSuKim1@users.noreply.github.com>
2025-06-12 18:32:53 +0900

    [2025-06-12] feature : 기초

--- ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,45 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.logging.Logger;
+
 public class JpaMain {
+
+    private static final Logger log = Logger.getLogger(JpaMain.class.getName());
 
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // Member 객체 생성
+            final Member memberToSave = new Member();
+            memberToSave.setId(1L);
+            memberToSave.setName("Kim");
+
+            // 저장
+            em.persist(memberToSave);
+
+            // 조회
+            Member member = em.find(Member.class, memberToSave.getId());
+            // 업데이트
+            member.setName("Jack");
+
+            // 삭제
+            em.remove(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            log.info(e.getMessage());
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
