6ab9578047727b19d992cf8021322f3291565dd5
Gyeongsu Kim <89643366+GyeongSuKim1@users.noreply.github.com>
2025-06-17 15:01:51 +0900

    [2025-06-17] refactor

--- ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
@@ -1,30 +1,40 @@
 package hellojpa;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Data
 public class Member {
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(name = "age")
+    private Integer age;
 
-    public String getName() {
-        return name;
-    }
+    @Column(name = "roleType")
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @Column(name = "modified_date")
+    private LocalDateTime modifiedDate;
+
+    @Lob
+    private String description;
 }
