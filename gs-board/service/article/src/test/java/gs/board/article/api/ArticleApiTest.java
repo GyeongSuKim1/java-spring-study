3c0dab748a76f3c7a05a05308aa81265fd29c327
Gyeongsu Kim <89643366+GyeongSuKim1@users.noreply.github.com>
2025-09-14 15:45:11 +0900

    Test : 게시판 CRUD테스트 및 목 데이터
    목 데이터 12,000,000 주입

---
+++ gs-board/service/article/src/test/java/gs/board/article/api/ArticleApiTest.java
@@ -0,0 +1,80 @@
+package gs.board.article.api;
+
+import gs.board.article.service.response.ArticleResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class ArticleApiTest {
+    RestClient restClient = RestClient.create("http://localhost:9000");
+
+    @Test
+    void createTest() {
+        final ArticleResponse response = create(
+                new ArticleCreateRequest("TEST Title", "TEST Content", 1L, 1L));
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void readTest() {
+        final ArticleResponse response = read(224806577060999168L);
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void updateTest() {
+        final ArticleResponse response = update(224426311831617536L, new ArticleUpdateRequest("Change Title2", "Change Content2"));
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void deleteTest() {
+        delete(224790700499861504L);
+    }
+
+    ArticleResponse create(final ArticleCreateRequest request) {
+        return restClient.post()
+                .uri("/v1/article")
+                .body(request)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    ArticleResponse read(final Long articleId) {
+        return restClient.get()
+                .uri("/v1/article/{articleId}", articleId)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    ArticleResponse update(final Long articleId, final ArticleUpdateRequest request) {
+        return restClient.put()
+                .uri("/v1/article/{articleId}", articleId)
+                .body(request)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    void delete(final Long articleId) {
+        restClient.delete()
+                .uri("/v1/article/{articleId}", articleId)
+                .retrieve();
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleCreateRequest {
+        private String title;
+        private String content;
+        private Long writerId;
+        private Long boardId;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleUpdateRequest {
+        private String title;
+        private String content;
+    }
+}
